8b7722fd1d4379f336428da708514ba6684ec4c5
Muhammad Fatih Yumna Lajuwirdi Lirrahman <m.fatihyumna25@gmail.com>
2023-05-10 09:04:51 +0700

    server edit🪁

--- webserver.py
+++ webserver.py
@@ -1,49 +1,64 @@
 import socket #import modul socket
 import os #import modul os
 
-# membuat socket TCP (1)
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #membuat objek socket TCP
+import socket
+import os
+
+# membuat socket TCP
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # mengaitkan socket ke alamat dan port tertentu
-server_address = ('localhost', 8080) #membuat objek server_address
-server_socket.bind(server_address) #mengaitkan server_socket ke server_address
+server_address = ('localhost', 8080)
+server_socket.bind(server_address)
 
 # mendengarkan koneksi masuk
-server_socket.listen(1) #mendengarkan koneksi masuk dengan parameter backlog 1
-print('Server is listening on port 8080...') #menampilkan pesan server sedang mendengarkan koneksi masuk
+server_socket.listen(1)
+print('Server is listening on port 8080...')
 
-while True: #looping forever
+while True:
     # menerima koneksi masuk
-    client_socket, client_address = server_socket.accept()#menerima koneksi masuk dan menyimpannya ke client_socket dan client_address
-    print(f'Connection from {client_address} has been established!')#menampilkan pesan koneksi telah berhasil dibuat
+    client_socket, client_address = server_socket.accept()
+    print(f'Connection from {client_address} has been established!')
 
     # menerima data dari client
-    data = client_socket.recv(1024).decode('utf-8')#menerima data dari client dan menyimpannya ke data
-    print(f'Received data from client: {data}')#menampilkan pesan data telah diterima
+    data = client_socket.recv(1024).decode('utf-8')
+    print(f'Received data from client: {data}')
 
-    # memparsing HTTP request(2)
-    request_method = data.split(' ')[0] #mengambil request method
-    request_file = data.split(' ')[1]#mengambil request file
+    # memparsing HTTP request
+    request_method = data.split(' ')[0]
+    request_file = data.split(' ')[1]
 
-    # mencari file yang diminta oleh client(3)
-    file_path = os.getcwd() + request_file #menggabungkan current working directory dengan request file
-    if os.path.isfile(file_path): #jika file ditemukan
+    # mencari file yang diminta oleh client
+    file_path = os.getcwd() + request_file
+    if os.path.isfile(file_path):
         # membuka file dan membacanya
-        with open(file_path, 'rb') as file:#membuka file dengan mode read binary
-            file_content = file.read()#membaca file dan menyimpannya ke file_content
-        # membuat HTTP response message(4)
+        with open(file_path, 'rb') as file:
+            file_content = file.read()
+        # membuat HTTP response message
         response = 'HTTP/1.1 200 OK\n\n' + file_content.decode('utf-8')
     elif request_file == "/500":
-        # membuat HTTP response message untuk error 500
-        response = 'HTTP/1.1 500 Internal Server Error\n\nError: Server Error'
+        # membuka file HTML error_500.html dan mengirimkan isinya sebagai respon HTTP
+        file_path = os.getcwd() + '/error_500.html'
+        if os.path.isfile(file_path):
+            with open(file_path, 'rb') as file:
+                file_content = file.read()
+            response = 'HTTP/1.1 500 Internal Server Error\n\n' + file_content.decode('utf-8')
+        else:
+            response = 'HTTP/1.1 404 Not Found\n\n404 Not Found'
     elif request_file == "/300":
-        # membuat HTTP response message untuk error 300
-        response = 'HTTP/1.1 300 Multiple Choices\n\nError: Multiple Choices'
+        # membuka file HTML error_300.html dan mengirimkan isinya sebagai respon HTTP
+        file_path = os.getcwd() + '/error_300.html'
+        if os.path.isfile(file_path):
+            with open(file_path, 'rb') as file:
+                file_content = file.read()
+            response = 'HTTP/1.1 300 Multiple Choices\n\n' + file_content.decode('utf-8')
+        else:
+            response = 'HTTP/1.1 404 Not Found\n\n404 Not Found'
     else:
-        # jika file tidak ditemukan, mengirimkan pesan "404 Not Found"(6)
+        # jika file tidak ditemukan, mengirimkan pesan "404 Not Found"
         response = 'HTTP/1.1 404 Not Found\n\n404 Not Found'
 
-    # mengirimkan response message ke client(5)
+    # mengirimkan response message ke client
     client_socket.sendall(response.encode('utf-8'))
 
     # menutup koneksi dengan client
